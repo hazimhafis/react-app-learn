33c4b191da7b3e2cbb039cad06ad8d0bd451aeaa
hajimekun <hazimmhf@gmail.com>
2022-02-15 14:47:56 +0800

    init project

---
+++ src/components/Expenses.js
@@ -0,0 +1,26 @@
+import ExpenseItem from "./ExpenseItem";
+import './Expenses.css';
+
+function Expenses(props) {
+	const expenses = props.items
+	// iterate through each object
+	console.table(expenses);
+	const listItems = expenses.map((item) => {
+		return (
+			<ExpenseItem
+				key={item.id}
+				title={item.title}
+				amount={item.amount}
+				date={item.date}
+			/>
+		)
+	});
+	return (
+		<div className="expenses">
+			{listItems}
+		</div>
+	)
+
+}
+
+export default Expenses;
