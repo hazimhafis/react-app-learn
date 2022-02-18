6e36a0ef66ff6e2ae776ebe9e7aa087554a9448f
hajimekun <hazimmhf@gmail.com>
2022-02-18 17:08:20 +0800

    change function syntax

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,6 +1,6 @@
 import './Card.css';
 
-function Card(props) {
+const Card = (props) => {
 	const classes = 'card ' + props.className;
 	// use props.children if want to create a wrapper component
 	return <div className={classes}>{props.children}</div>
