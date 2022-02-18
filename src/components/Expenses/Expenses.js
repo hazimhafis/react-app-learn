d84989eb0db51175c22c0eeda9a7776254cfa1eb
hajimekun <hazimmhf@gmail.com>
2022-02-18 17:07:02 +0800

    change function syntax

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,7 +2,7 @@ import ExpenseItem from "./ExpenseItem";
 import Card from '../UI/Card';
 import './Expenses.css';
 
-function Expenses(props) {
+const Expenses = (props) => {
 	const expenses = props.items
 	// iterate through each object
 	const listItems = expenses.map((item) => {
